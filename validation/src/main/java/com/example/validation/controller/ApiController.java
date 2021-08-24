bc360cf574a960718219680f7e85413109dfe952
jinseonghee <wlstjdgml0610@gmail.com>
2021-08-24 14:26:06 +0900

    custom validation (2)

--- validation/src/main/java/com/example/validation/controller/ApiController.java
+++ validation/src/main/java/com/example/validation/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.example.validation.controller;
 
 import com.example.validation.dto.User;
+import com.example.validation.dto.User1;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
